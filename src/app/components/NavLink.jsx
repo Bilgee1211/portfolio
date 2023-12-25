5f732d54789e4fabe6544a1c9ac442dd33367804
Bilegbaatar <bilgeebek1211@gmail.com>
2023-12-26 02:54:13 +0900

    changed class property to className

--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const NavLink = ({ href, title }) => {
   return (
-    <span class="max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600">
+    <span className="max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600">
       <Link
         href={href}
         className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white group transition duration-200"
